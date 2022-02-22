820f67e8d0b69bb5a767dc1be006ef521766f390
sumeetpatelofficial <38639356+sumeetpatelofficial@users.noreply.github.com>
2022-02-22 07:35:33 +0530

    first program for react

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./components/Header";
+import About from "./components/About";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <About />
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    colors: {
+      red: "#DA1212",
+      blue: "#11468F",
+      purple: "#512D6D",
+      pink: "#ff49db",
+      orange: "#ff7849",
+      green: "#13ce66",
+      yellow: "#ffc82c",
+      "gray-dark": "#273444",
+      gray: "#8492a6",
+      "gray-light": "#d3dce6",
+      white: "#ffffff",
+      teal: "#24A19C",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
